7e756048baa4aa6d2a492e1da038847ac7c80223
rarmax00 <160176620+rarmax00@users.noreply.github.com>
2025-01-08 20:51:03 +0200

    Update Passenger.java

--- lab6/src/Passenger.java
+++ lab6/src/Passenger.java
@@ -1,1 +1,33 @@
+package ki_304.Hertsyk.Lab6;
+
+/**
+ * Клас, що представляє пасажира.
+ */
+public class Passenger {
+    private String name;
+    private int age;
+
+    /**
+     * Конструктор пасажира.
+     * @param name Ім'я пасажира.
+     * @param age Вік пасажира.
+     */
+    public Passenger(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    /**
+     * Повертає вік пасажира.
+     * @return Вік пасажира.
+     */
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Passenger{name='" + name + "', age=" + age + "}";
+    }
+}
 
