22cf461203de40c0bf900709cbe0c6541ab498a2
rarmax00 <160176620+rarmax00@users.noreply.github.com>
2025-01-08 20:41:31 +0200

    Update WorkStationDriver.java

--- lab2/src/WorkStationDriver.java
+++ lab2/src/WorkStationDriver.java
@@ -1,1 +1,38 @@
+package ki_304.Hertsyk.Lab2;
+
+import java.io.IOException;
+
+/**
+ * Клас WorkstationDriver є драйвером для демонстрації роботи класу Workstation.
+ * Виконує основні операції з комп'ютером і монітором, такі як увімкнення/вимкнення та перевірка стану.
+ */
+public class WorkstationDriver {
+
+    /**
+     * Метод main є точкою входу в програму. Він демонструє основну функціональність класу Workstation.
+     *
+     * @param args аргументи командного рядка (не використовуються).
+     */
+    public static void main(String[] args) {
+        try {
+            // Створюємо робочу станцію
+            Workstation ws = new Workstation();
+
+            // Увімкнення комп'ютера та монітора
+            ws.turnOn();
+
+            // Перевірка стану робочої станції
+            ws.inspectWorkstation();
+
+            // Вимкнення комп'ютера та монітора
+            ws.turnOff();
+
+            // Закриваємо логер
+            ws.closeLogger();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
 
