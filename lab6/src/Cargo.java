77645e693ebb3c79237907bec6aceed1da3c1030
rarmax00 <160176620+rarmax00@users.noreply.github.com>
2025-01-08 20:50:38 +0200

    Update Cargo.java

--- lab6/src/Cargo.java
+++ lab6/src/Cargo.java
@@ -1,1 +1,34 @@
+package ki_304.Hertsyk.Lab6;
+
+/**
+* Клас, що представляє вантаж.
+*/
+public class Cargo {
+private String name;
+private double weight;
+
+/**
+ * Конструктор вантажу.
+ *
+ * @param name   Назва вантажу.
+ * @param weight Вага вантажу.
+ */
+public Cargo(String name, double weight, double v) {
+    this.name = name;
+    this.weight = weight;
+}
+
+/**
+ * Повертає вагу вантажу.
+ * @return Вага вантажу.
+ */
+public double getWeight() {
+    return weight;
+}
+
+@Override
+public String toString() {
+    return "Cargo{name='" + name + "', weight=" + weight + "}";
+}
+}
 
