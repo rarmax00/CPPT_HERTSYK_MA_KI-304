4e11998599530df8bef7404b2df22cc671202d3d
rarmax00 <160176620+rarmax00@users.noreply.github.com>
2025-01-08 20:45:07 +0200

    Update Portable.java

--- lab3/src/Portable.java
+++ lab3/src/Portable.java
@@ -1,1 +1,12 @@
+package ki_304.Hertsyk.Lab_3;
+
+/**
+ * Інтерфейс Portable визначає, що пристрій може бути портативним.
+ */
+public abstract interface Portable {
+    /**
+     * Метод, що повертає інформацію про портативність пристрою.
+     */
+    void carry();
+}
 
